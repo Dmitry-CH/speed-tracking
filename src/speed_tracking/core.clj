27b3358db2dea79c521a95b12b4e8eaf21053635
Dmitry Chekanov <chd@kodix.ru>
2021-12-17 17:38:12 +0300

    add server dummy

--- src/speed_tracking/core.clj
+++ src/speed_tracking/core.clj
@@ -1,7 +1,22 @@
 (ns speed-tracking.core
+  (:require [ring.adapter.jetty :refer [run-jetty]]
+            [speed-tracking.handler :refer [app*]])
   (:gen-class))
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+(defonce server (atom nil))
+
+(def server-opts {:port 8000
+                  :join? false})
+
+(defn start-server []
+  (reset! server
+          (run-jetty app* server-opts)))
+
+(defn stop-server []
+  (when-some [s @server]
+    (.stop s)
+    (reset! server nil)))
+
+(comment
+  (start-server)
+  (stop-server))
---
+++ src/speed_tracking/handler.clj
@@ -0,0 +1,6 @@
+(ns speed-tracking.handler)
+
+(defn app* [request]
+  {:status 200
+   :headers {"Content-Type" "text/html"}
+   :body "Hello World!"})
