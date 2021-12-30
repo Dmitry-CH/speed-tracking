089b156a3f8667bb3f9df8e634fbb67a19538a1b
Dmitry Chekanov <chd@kodix.ru>
2021-12-30 15:43:34 +0300

    add deps in project

--- project.clj
+++ project.clj
@@ -2,9 +2,12 @@
   :description "Websites loading speed tracking app"
   :url "https://gitlab.kodixauto.ru/oneplatform-frontend/speed-tracking"
   :dependencies [[org.clojure/clojure "1.10.3"]
+                 [com.novemberain/monger "3.1.0"]
                  [compojure "1.6.2" :exclusions [ring/ring-codec]]
                  [ring/ring-core "1.9.4"]
+                 [ring/ring-devel "1.9.4"]
                  [ring/ring-jetty-adapter "1.9.4"]]
+  :min-lein-version "2.0.0"
   :main ^:skip-aot speed-tracking.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all
