7ac86b61a46e26db7ccdf166da25f1c779fe77c6
Dmitry Chekanov <chd@kodix.ru>
2022-02-04 17:53:52 +0300

    log setup

--- project.clj
+++ project.clj
@@ -1,10 +1,12 @@
 (defproject speed-tracking "0.1.0-SNAPSHOT"
   :description "Websites loading speed tracking app"
   :url "https://gitlab.kodixauto.ru/oneplatform-frontend/speed-tracking"
-  :dependencies [[org.clojure/clojure "1.10.3"]
+  :dependencies [[ch.qos.logback/logback-classic "1.2.10"]
                  [com.novemberain/monger "3.5.0"]
                  [compojure "1.6.2" :exclusions [ring/ring-codec]]
                  [environ "1.2.0"]
+                 [org.clojure/clojure "1.10.3"]
+                 [org.clojure/tools.logging "1.2.4"]
                  [ring/ring-core "1.9.5"]
                  [ring/ring-devel "1.9.5"]
                  [ring/ring-jetty-adapter "1.9.5"]
